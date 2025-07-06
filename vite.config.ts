849fd3262ad80e6e0a322ac360de718a66dcf564
sajidiftekhar <ifti.sajid@gmail.com>
2025-07-06 06:02:03 +0600

    add intro

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
     const env = loadEnv(mode, process.cwd(), '')
 
     return defineConfig({
-        plugins: [react(), tailwindcss()],
+        plugins: [react()],
         base: env.VITE_BASENAME || '/',
     })
 }
